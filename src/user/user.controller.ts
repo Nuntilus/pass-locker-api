a0ac28ff5cd422ee10ca9ca27fb2a6012c7046a5
Mg131209 <miogalli@icloud.com>
2026-05-10 12:28:31 +0200

    feat: add basic create endpoint for user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserPayload } from './payload/create-user.payload';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserPayload) {
+    return this.userService.create(createUserDto);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserPayload } from './payload/create-user.payload';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly database: PrismaService){}
+  async create(user: CreateUserPayload) { 
+    return this.database.user.create({
+      data:{...user}
+    })
+  }
+}
