5dc51b23efe60d6fb7ea3e42a9a3c1aa2de7f6ec
Mg131209 <miogalli@icloud.com>
2026-05-11 08:43:57 +0200

    docs: add JSdocs

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,16 +6,30 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class UserService {
   constructor(private readonly database: PrismaService) { }
 
+  /**
+   * Creates the given user in the Database
+   * @param user  The user to create
+   * @returns     THe created user
+   */
   async create(user: CreateUserPayload) {
     return this.database.user.create({
       data: user,
     });
   }
 
+  /**
+   * Finds the user with the given uuid 
+   * @param uuid  The uuid to fid the user with
+   * @returns The found user
+   */
   findByUuid(uuid: string) {
     return this.database.user.findUnique({ where: { uuid } });
   }
 
+  /**
+   * Lists al user in the database
+   * @returns All found users
+   */
   list() {
     return this.database.user.findMany();
   }
